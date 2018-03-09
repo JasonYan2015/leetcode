9317adf4721809c2f31a95c44db53f8fbbe89675
Ajun <yanzijun96@163.com>
2018-03-09 10:12:36 +0800

    add No.16

--- 16. 3Sum Closest.js
+++ 16. 3Sum Closest.js
@@ -6,18 +6,45 @@
  * @return {number}
  */
 var threeSumClosest = function(nums, target) {
-  const 
-    resArr = []
+  let 
+    closetRes = 0,
+    closeDistance = +Infinity
 
-  nums.map((val, index) => {
-    const
-      tar = target - val,
-      newArr = nums
+  nums
+    .sort((a, b) => a - b)
+    .map((val, index) => {
+      const tar = target - val
+      const newArr = nums
         .slice(0, index)
         .concat(
           nums.slice(index + 1)
         )
 
+      let 
+        i = 0,
+        j = newArr.length - 1
+
+      while(i < j && closeDistance !== 0) {
+        const res = tar - newArr[i] - newArr[j]
+        if (Math.abs(res) < closeDistance) {
+          closetRes = newArr[i] + newArr[j] + val
+          closeDistance = Math.abs(res)
+        }
+        if (res < 0) j--
+        else i++
+      }
+    })
     
-  })
+  return closetRes
 };
+
+/**
+ * 测试用例
+ * 答案应该是 -1
+ */
+const
+  nums = [1,1,-1,-1,3],
+  target = -1,
+  res = threeSumClosest(nums, target)
+
+console.log('--------------', res)
