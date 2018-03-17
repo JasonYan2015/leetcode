fe2b919e45398b0f4829fe0e4de2b955d61b7161
Ajun <yanzijun96@163.com>
2018-03-17 22:37:12 +0800

    add No.10

--- 10. Regular Expression Matching.js
+++ 10. Regular Expression Matching.js
@@ -4,6 +4,37 @@
  * @param {string} p
  * @return {boolean}
  */
-var isMatch = function(s, p) {
-    
+const isMatch = function (s, p) {
+  const
+    arr = s.split(''),
+    exArr = p.split('')
+
+  let
+    index = 0,
+    exIndex = 0
+
+  for (; index < arr.length; index++) {
+    const val = arr[index]
+
+    if (val === exArr[exIndex] || exArr[exIndex] || exArr[exIndex + 1] === '*') {
+      exIndex += exArr[exIndex + 1] === '*' ? 2 : 1
+    } else {
+      break
+    }
+  }
+
+  const lastExFlag = exArr
+    .slice(exIndex)
+    .join('')
+    .split('*')
+    .every(val => val.length === 1)
+
+  return index === arr.length && lastExFlag
 };
+
+const
+  s = 'aab',
+  p = 'c*a*b',
+  res = isMatch(s, p)
+
+console.log('----------------', res)
