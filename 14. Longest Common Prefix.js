e1203ea15fdc471126514153be0fbb4bc635d1ac
Ajun <yanzijun96@163.com>
2018-05-12 23:18:57 +0800

    MOD. 完成No.14

--- 14. Longest Common Prefix.js
+++ 14. Longest Common Prefix.js
@@ -14,5 +14,40 @@
  * @return {string}
  */
 const longestCommonPrefix = function(strs) {
-    
+  const 
+    arr = strs,
+    lastIndex = arr.length - 1
+
+  let
+    common = [],
+    flag = true,
+    newChar = ''
+
+  while(flag && arr.length > 0) {
+    for (let i = 0; i < arr.length; i++) {
+      const 
+        str = arr[i],
+        firstLetter = str.charAt(0)
+
+      if (str === '' || (i > 0 && firstLetter !== newChar)) {
+          flag = false
+          break
+        }
+      if (i === 0) newChar = firstLetter
+
+      arr[i] = str.slice(1)
+
+      if (i === lastIndex) {
+        common.unshift(str.charAt(0))
+      }
+    }
+  }
+
+  return common.reverse().join('')
 };
+
+const
+  strs = [""],
+  res = longestCommonPrefix(strs)
+
+console.log('res: ----------', res)
