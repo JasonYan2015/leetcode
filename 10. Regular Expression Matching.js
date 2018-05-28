f87b36fb31b85128ccfb5133f6ddb7a02a8118b2
Ajun <yanzijun96@163.com>
2018-05-28 16:42:11 +0800

    MOD. reset No.10

--- 10. Regular Expression Matching.js
+++ 10. Regular Expression Matching.js
@@ -1,40 +1,25 @@
 /**
+ * Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
+ * '.' Matches any single character.
+ * '*' Matches zero or more of the preceding element.
+ * The matching should cover the entire input string (not partial).
+ * 
+ * Note:
+ * s could be empty and contains only lowercase letters a-z.
+ * p could be empty and contains only lowercase letters a-z, and characters like . or *.
+ * 
  * Implement regular expression matching with support for '.' and '*'.
  * @param {string} s
  * @param {string} p
  * @return {boolean}
  */
 const isMatch = function (s, p) {
-  const
-    arr = s.split(''),
-    exArr = p.split('')
 
-  let
-    index = 0,
-    exIndex = 0
-
-  for (; index < arr.length; index++) {
-    const val = arr[index]
-
-    if (val === exArr[exIndex] || exArr[exIndex] === '.' || exArr[exIndex + 1] === '*') {
-      exIndex += exArr[exIndex + 1] === '*' ? 2 : 1
-    } else {
-      break
-    }
-  }
-
-  const lastExFlag = exArr
-    .slice(exIndex)
-    .join('')
-    .split('*')
-    .every(val => val.length <= 1)
-
-  return index === arr.length && lastExFlag
 };
 
 const
   s = 'aab',
-  p = 'c*a*b',
+  p = 'c*ab',
   res = isMatch(s, p)
 
 console.log('----------------', res)
