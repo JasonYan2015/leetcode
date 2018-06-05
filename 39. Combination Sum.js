13c4daffe71729b4edf529f4979a04ba1bc8e8e7
Ajun <yanzijun96@163.com>
2018-06-05 14:15:28 +0800

    MOD. 完成 No.39

--- 39. Combination Sum.js
+++ 39. Combination Sum.js
@@ -9,6 +9,39 @@
  * @param {number} target
  * @return {number[][]}
  */
-const combinationSum = function(candidates, target) {
-    
+const combinationSum = function (candidates, target) {
+  const
+    arr = candidates.sort((a, b) => a - b),
+    res = []
+
+  for (let i = 0; i < arr.length && arr[i] <= target; i++) {
+    const
+      val = arr[i],
+      s = new Array()
+
+    for (let j = Math.floor(target / val); j > 0; j--) {
+      const newTar = target - val * j
+      s.length = j
+      s.fill(val, 0)
+
+      if (newTar === 0) {
+        res.push(s.slice(0))
+        continue
+      }
+
+      if(i === 0) break
+      combinationSum(arr.slice(0, i), newTar).map(resArr => {
+        res.push(s.concat(resArr))
+      })
+    }
+  }
+
+  return res
 };
+
+const
+  candidates = [3,12,9,11,6,7,8,5,4],
+  target = 15,
+  res = combinationSum(candidates, target)
+
+console.log('res: -------------------------------', res)
